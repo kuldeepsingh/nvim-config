cbd2310b817c64cfffbb0647964ebfd535e5adaa
Kuldeep Singh <kuldeep.singh.chahar@gmail.com>
2025-01-24 19:18:40 +0530

    adding python debugger

--- init.lua
+++ init.lua
@@ -201,6 +201,35 @@ require("nvim-tree").setup {
     ---
 }
 
+require("dap-python").setup("/Users/kuldeepsingh/.virtualenvs/debugpy/bin/python")
+
+-- nvim dap mappings
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
+vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
+vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
+vim.keymap.set('n', '<C-b>', function() require('dap').toggle_breakpoint() end)
+
+local dap, dapui = require("dap"), require("dapui")
+dapui.setup()
+
+-- open Dap UI automatically when debug starts (e.g. after <F5>)
+dap.listeners.before.attach.dapui_config = function()
+    dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+    dapui.open()
+end
+
+-- close Dap UI with :DapCloseUI
+vim.api.nvim_create_user_command("DapCloseUI", function()
+    require("dapui").close()
+end, {})
+
+-- use <CTRL-e> to eval expressions
+vim.keymap.set({ 'n', 'v' }, '<C-e>', function() require('dapui').eval() end)
+
+
 vim.schedule(function()
     require "mappings"
 end)
