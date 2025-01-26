0337019d0344c278d0ccfcb072dc190789197626
Kuldeep Singh <kuldeep.singh.chahar@gmail.com>
2025-01-26 23:51:57 +0530

    cmdline

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -265,4 +265,26 @@ return {
             vim.g.VM_add_cursor_at_pos_no_mappings = 1
         end,
     },
+    -- lazy.nvim
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add any options here
+        },
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        },
+    },
+    {
+        "linrongbin16/gentags.nvim",
+        config = function()
+            require("gentags").setup()
+        end,
+    },
 }
