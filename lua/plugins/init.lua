bb2e14693c98f4537aa597f12671ecb7e318544a
kuldeep singh <kuldeep.singh.chahar@gmail.com>
2025-02-18 07:32:58 +0530

    dummy for checkin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1079,4 +1079,20 @@ return {
         "0x00-ketsu/maximizer.nvim",
     },
     ---------------------------------------------------------------------------
+
+    ---------------------------------------------------------------------------
+    --  Bufferlist using <leader>bb
+    ---------------------------------------------------------------------------
+    {
+        "EL-MASTOR/bufferlist.nvim",
+        lazy = true,
+        keys = { { "<Leader>bb", ":BufferList<CR>", desc = "Open bufferlist" } },
+        dependencies = "nvim-tree/nvim-web-devicons",
+        cmd = "BufferList",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+    },
 }
