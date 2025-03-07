bda2155f5c903f3fe4e1e1c0235eafeb7a356fb3
kuldeepsingh <kuldeep.singh.chahar@gmail.com>
2025-03-07 19:18:59 +0530

    checkin from url opening changes incognito mode

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1148,4 +1148,38 @@ return {
             "nvim-treesitter/nvim-treesitter",
         },
     },
+
+    {
+        "MarcHamamji/runner.nvim",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            dependencies = { "nvim-lua/plenary.nvim" },
+        },
+    },
+
+    {
+        "Mr-LLLLL/interestingwords.nvim",
+    },
+
+    {
+        "jyscao/ventana.nvim",
+    },
+
+    {
+        "cappyzawa/trim.nvim",
+        opts = {},
+    },
+
+    {
+        "sontungexpt/url-open",
+        event = "VeryLazy",
+        cmd = "URLOpenUnderCursor",
+        config = function()
+            local status_ok, url_open = pcall(require, "url-open")
+            if not status_ok then
+                return
+            end
+            url_open.setup {}
+        end,
+    },
 }
